d33e615b2429a306d206b51902c877fc9c7c1b5b
Will Ricketts <will@willricketts.com>
2016-02-09 00:19:20 -0500

    removing comment

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ func main() {
     router := gin.Default()
     router.Static("/assets", "./bower_components")
     router.LoadHTMLGlob("public/*")
-    //router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
     router.GET("/", func(c *gin.Context) {
         c.HTML(http.StatusOK, "index.html", gin.H{
             "title": "Main website",
