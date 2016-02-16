b1a290806671d142837dd56371f19245f7d4f886
Will Ricketts <will@willricketts.com>
2016-02-16 15:26:35 -0500

    fixing server and modifying style

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ import "github.com/gin-gonic/gin"
 import "net/http"
 
 func main() {
-    router := gin.Default()
-    router.Static("/assets", "./bower_components")
-    router.Static("/styles", "./public/styles")
-    router.LoadHTMLGlob("public/*")
-    router.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.html", gin.H{
-            "title": "Main website",
-        })
-    })
-    router.Run(":8080")
+	router := gin.Default()
+	router.Static("/assets", "./bower_components")
+	router.Static("/styles", "./public/styles")
+	router.LoadHTMLGlob("public/*.html")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	router.Run(":8080")
 }
